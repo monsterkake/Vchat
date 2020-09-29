82dd700af10b5ab5ab49b3d0f415adfec8be5139
monsterkake <56121889+monsterkake@users.noreply.github.com>
2020-09-29 20:18:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 
 app.use(express.static(__dirname + "/public"))
 let clients = 0
